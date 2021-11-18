96e0ece170984f948c1b24601d790fbda2ae4983
DinhRi <ritrandinh124@gmail.com>
2021-11-18 19:08:51 +0700

    them hieu

--- source.cpp
+++ source.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 using namespace std;
 int tong(int a, int b);
-int hieu(int a, int b);
+int hieu(int a, int b)
+{
+	return a-b;
+}
 
 int main()
 {
-	int a, b;
+	int a, b,ketqua;
 	char phepToan;
 	cout << "Nhap so nguyen 1: ";
 	cin >> a;
@@ -13,6 +16,9 @@ int main()
 	cin >> b;
 	cout << "Chon phep toan (+,-) ";
 	cin >> phepToan;
+	if(phepToan=="-")
+		ketqua=hieu(a,b);
+	cout<<"ket qua : " + ketqua;
 	system("pause");
 	return 0;
 }
